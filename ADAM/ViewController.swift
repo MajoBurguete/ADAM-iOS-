f651b6a24d6bed3f284a9606672e00d68a5539b4
Majo Burguete <marijo.mjbe@gmail.com>
2021-08-28 23:34:02 -0500

    Initial Commit

---
+++ ADAM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ADAM
+//
+//  Created by Maria jose Burguete euan on 28/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
