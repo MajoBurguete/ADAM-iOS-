03767def95bdffc921d7a99c2e36293c29b25351
Majo Burguete <marijo.mjbe@gmail.com>
2021-10-16 19:02:24 -0500

    Story Model progress

--- ADAM/ViewControllers/StoryViewController.swift
+++ ADAM/ViewControllers/StoryViewController.swift
@@ -9,6 +9,33 @@ import UIKit
 
 class StoryViewController: UIViewController {
     
+    @IBOutlet weak var tvStory: UITextView!
+    
+    
+    @IBOutlet weak var ivLife1S: UIImageView!
+    
+    @IBOutlet weak var ivLife2S: UIImageView!
+    
+    @IBOutlet weak var ivLife3S: UIImageView!
+    
+    @IBOutlet weak var btnContinue: UIButton!
+    
+    @IBOutlet weak var tvQuestion: UITextView!
+    
+    @IBOutlet weak var tvScoreS: UILabel!
+    
+    @IBOutlet weak var tvResult: UILabel!
+    
+    @IBOutlet weak var btnFirstOption: UIButton!
+    
+    @IBOutlet weak var btnSecondOption: UIButton!
+    
+    
+    @IBOutlet weak var btnThirdOption: UIButton!
+    
+    @IBOutlet weak var btnNextQ: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
